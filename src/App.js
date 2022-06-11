d57dd1626a020a9e433d35c51cba8aa95408762e
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-06-10 21:21:06 -0400

    final production commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeandLocation from "./components/TimeandLocation";
+import TemperatureDetails from "./components/TemperatureDetails";
+import Forecast from "./components/Forecast.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import getFormattedWeatherData from "./components/services/WeatherService";
 
 function App() {
+  const [query, setQuery] = useState({ q: "Toronto" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-300 to to-blue-800";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-300 to to-blue-800";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+      {/* Middle of the Nav */}
+
+      {weather && (
+        <div>
+          <TimeandLocation weather={weather} />
+          <TemperatureDetails weather={weather} />
+          <Forecast title="Hourly Forecast" items={weather.hourly} />
+          <Forecast title="Daily Forecast" items={weather.daily} />
+        </div>
+      )}
+
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
     </div>
   );
 }
